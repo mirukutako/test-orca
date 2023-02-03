c788b2a25e80d1f5c97f7ad9c5c90f7764f6326a
mirukutako <deemakoo@gmail.com>
2023-02-03 19:58:17 +0300

    add Global styles

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
+import { createGlobalStyle } from 'styled-components'
 import Home from 'modules/Home/Home'
 
+const GlobalyStyles = createGlobalStyle`
+  html {
+    --space: 8px;
+    --main-color: #10111D;
+    --gray-color: #CACFDB;
+    --caption-color: #70707B;
+    --accent-color: #0080FF;
+    --high-color: #E2005E;
+    --medium-color: #FE7B1F;
+    --low-color: #F8CD39;
+
+    color: var(--main-color);
+  }
+`
+
 const App = () => (
-  <div>
+  <>
+    <GlobalyStyles />
     Hello <Home />
-  </div>
+  </>
 )
 
 export default App
