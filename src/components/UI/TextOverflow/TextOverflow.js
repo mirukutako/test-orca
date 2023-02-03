76e7cb58c6ba34f36f26af9afb04f6f5905db8ff
mirukutako <deemakoo@gmail.com>
2023-02-03 21:20:57 +0300

    add TextOverflow component

---
+++ src/components/UI/TextOverflow/TextOverflow.js
@@ -0,0 +1,5 @@
+import { Wrapper } from './TextOverflow.styles'
+
+const TextOverflow = ({ children }) => <Wrapper>{children}</Wrapper>
+
+export default TextOverflow
---
+++ src/components/UI/TextOverflow/TextOverflow.styles.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.span`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+`
