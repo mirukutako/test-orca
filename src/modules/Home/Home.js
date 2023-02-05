482d486988232d03c6ad06bfd04e73dce22be467
mirukutako <deemakoo@gmail.com>
2023-02-05 16:06:58 +0300

    add template

--- src/modules/Home/Home.js
+++ src/modules/Home/Home.js
@@ -1,11 +1,29 @@
+import { useState } from 'react'
+import Catalog from 'components/Catalog/Catalog'
 import Container from 'components/Container/Container'
 import Header from 'components/Header/Header'
-const Home = () => (
-  <div>
-    <Container>
-      <Header />
-    </Container>
-  </div>
-)
+import { IcTable, IcCard } from 'components/UI/Icons/Icons'
+
+const Home = () => {
+  const items = [
+    { type: 'table', icon: <IcTable /> },
+    { type: 'card', icon: <IcCard /> },
+  ]
+
+  const [view, setView] = useState('table')
+
+  const changeView = view => {
+    setView(view)
+  }
+
+  return (
+    <div>
+      <Container>
+        <Header viewNav={items} viewActive={view} viewHandleClick={changeView} />
+        <Catalog viewActive={view} />
+      </Container>
+    </div>
+  )
+}
 
 export default Home
