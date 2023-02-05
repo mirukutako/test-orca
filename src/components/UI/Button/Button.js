f6d64db81950c7954b57c36f07a8c64cbd05558b
mirukutako <deemakoo@gmail.com>
2023-02-05 20:45:46 +0300

    rework Button

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,5 +1,20 @@
 import { Wrapper } from './Button.styles'
 
-const Button = ({ children }) => <Wrapper>{children}</Wrapper>
+const Button = ({ children, handleModalVisible, sendData, primary }) => {
+  const btnClick = () => {
+    if (handleModalVisible) {
+      handleModalVisible(true, {
+        status: sendData && sendData.status,
+        name: sendData && sendData.file.name,
+        additional: sendData && sendData.additional,
+      })
+    }
+  }
+  return (
+    <Wrapper onClick={btnClick} primary={primary}>
+      {children}
+    </Wrapper>
+  )
+}
 
 export default Button
--- src/components/UI/Button/Button.styles.js
+++ src/components/UI/Button/Button.styles.js
@@ -1,13 +1,15 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.button`
+  height: 30px;
   display: inline-flex;
   align-items: center;
   gap: var(--space);
   border: none;
-  background-color: var(--accent-color);
-  color: #fff;
+  background-color: ${props => (props.primary ? 'white' : 'var(--accent-color)')};
+  color: ${props => (props.primary ? 'var(--main-color)' : 'white')};
   padding: 6px var(--space);
+  border: ${props => (props.primary ? '1px solid var(--gray-color)' : 'none')};
   border-radius: 4px;
   position: relative;
   z-index: 1;
@@ -21,7 +23,7 @@ export const Wrapper = styled.button`
     bottom: 0;
     z-index: 1;
     border-radius: 4px;
-    box-shadow: 0px 0px 0px 2px #e1eefd;
+    box-shadow: 0px 0px 0px 2px ${props => (props.primary ? 'var(--accent-color)' : '#e1eefd')};
     opacity: 0;
     transition: opacity 0.2s ease;
   }
